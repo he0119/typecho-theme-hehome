42889f8b70f67e2654c3029f66804c4ecb34d74f
Mengyang He <hmy0119@gmail.com>
2018-11-25 23:44:36 +0800

    修复主页也显示noindex的问题

--- hehome/header.php
+++ hehome/header.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <?php if ($this->hidden): ?>
+    <?php if ($this->password): ?>
         <meta name="robots" content="noindex">
     <?php endif; ?>
     <title><?php $this->archiveTitle(array(
