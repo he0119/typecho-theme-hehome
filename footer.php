ce3d189b25dea41cb826e07c5d0cf0f060b9f77b
uy_sun <hmy0119@gmail.com>
2023-10-21 21:44:39 +0800

    feat: 更新备案网址与图片

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@
     <!-- 备案信息 -->
     <div class="blog-text-center">
         <a href="https://beian.miit.gov.cn/" target="_blank" rel="nofollow noopener">蜀ICP备18034005号-1</a>
-        <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51011202000228" target="_blank" rel="nofollow noopener">
-            <img src="<?php $this->options->themeUrl('img/beian.webp'); ?>" alt="beian" loading="lazy" width="20" height="20"/>
-            <span>川公网安备 51011202000228号</span>
+        <a href="https://beian.mps.gov.cn/#/query/webSearch?code=51011202000228" target="_blank" rel="nofollow noopener">
+            <img src="<?php $this->options->themeUrl('img/beian.png'); ?>" alt="beian" loading="lazy" width="16" height="17"/>
+            <span>川公网安备51011202000228号</span>
         </a>
     </div>
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
